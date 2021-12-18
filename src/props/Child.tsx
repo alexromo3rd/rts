71a1f198b6a36be3ca98649e99a948be23731695
Alex Romo III <alexromo3rd@gmail.com>
2021-12-17 21:37:09 -0600

    Working through Annotations with Children video

---
+++ src/props/Child.tsx
@@ -0,0 +1,23 @@
+interface ChildProps {
+  color: string;
+  onClick: () => void;
+}
+
+export const Child = ({ color, onClick }: ChildProps) => (
+  <div>
+    {color}
+    <button onClick={onClick}>Click me</button>
+  </div>
+);
+
+// Benefits of using this approach
+// 1) Specifying it is a React Functional Component
+// 2) Component might have some additional properties assigned to it
+// like 'propTypes' and 'contextTypes'
+// 3) Specifying it will receive props of type ChildProps
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick }) => (
+  <div>
+    {color}
+    <button onClick={onClick}>Click me</button>
+  </div>
+);
---
+++ src/props/Parent.tsx
@@ -0,0 +1,5 @@
+import { Child } from './Child';
+
+const Parent = () => <Child color='blue' onClick={() => 'hi'} />;
+
+export default Parent;
