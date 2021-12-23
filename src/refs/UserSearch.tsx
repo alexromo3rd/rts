6ddc138e2f53386b85e3e6b4321f668fe67f1ac6
Alex Romo III <alexromo3rd@gmail.com>
2021-12-23 17:51:11 -0600

    TypeScript and Redux

--- src/refs/UserSearch.tsx
+++ src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const users = [
   {
@@ -16,8 +16,15 @@ const users = [
 ];
 
 const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [name, setName] = useState('');
   const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
 
   const onClick = () => {
     const foundUser = users.find(user => {
@@ -28,7 +35,11 @@ const UserSearch: React.FC = () => {
 
   return (
     <div>
-      <input value={name} onChange={e => setName(e.target.value)} />
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
       <button onClick={onClick}>Find User</button>
       {user ? (
         <p>
